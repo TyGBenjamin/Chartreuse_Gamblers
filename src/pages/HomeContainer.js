2171f8c7fa9ccd600c04b46a56544618d699fa4d
Martin Corr <martinjcorr2@gmail.com>
2022-05-05 21:43:58 -0400

    added signup page

--- src/pages/HomeContainer.js
+++ src/pages/HomeContainer.js
@@ -1,39 +1,38 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import HomePage from "./HomePage";
-import Login from "./Login";
+import Login from "./login";
 import Profile from "./Profile";
 import SignUp from "./SignUp";
 import Sports from "./Sports";
 
 export default function HomeContainer() {
-    const [currentPage, setCurrentPage] = useState('HomePage');
+  const [currentPage, setCurrentPage] = useState("HomePage");
 
-    const renderPage = () => {
-        if (currentPage === "HomePage") {
-            return <HomePage />
-        }
-        if (currentPage === "Login") {
-            return <Login />
-        }
-        if (currentPage === "SignUp") {
-            return <SignUp />
-        }
-        if (currentPage === "Profile") {
-            return <Profile />
-        }
-        if (currentPage === "Sports") {
-            return <Sports />
-        }
-    };
+  const renderPage = () => {
+    if (currentPage === "HomePage") {
+      return <HomePage />;
+    }
+    if (currentPage === "Login") {
+      return <Login />;
+    }
+    if (currentPage === "SignUp") {
+      return <SignUp />;
+    }
+    if (currentPage === "Profile") {
+      return <Profile />;
+    }
+    if (currentPage === "Sports") {
+      return <Sports />;
+    }
+  };
 
-    const handlePageChange = (page) => setCurrentPage(page);
+  const handlePageChange = (page) => setCurrentPage(page);
 
-    return(
-        <div>
-        <Header currentPage={currentPage}
-        handlePageChange={handlePageChange}/>
-        {renderPage()}
-        </div>
-    )
-};
+  return (
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,158 @@
-import React from 'react';
+import React from "react";
 
-function SignUp() {
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {},
+      errors: {},
+    };
+  }
+
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Name
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "Cannot be empty";
+    }
+
+    if (typeof fields["name"] !== "undefined") {
+      if (!fields["name"].match(/^[a-zA-Z]+$/)) {
+        formIsValid = false;
+        errors["name"] = "Only letters";
+      }
+    }
+
+    //Name
+    if (!fields["lastName"]) {
+      formIsValid = false;
+      errors["lastName"] = "Cannot be empty";
+    }
+
+    if (typeof fields["lastName"] !== "undefined") {
+      if (!fields["lastName"].match(/^[a-zA-Z]+$/)) {
+        formIsValid = false;
+        errors["lastName"] = "Only letters";
+      }
+    }
+
+    //Email
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+
+    if (typeof fields["email"] !== "undefined") {
+      let lastAtPos = fields["email"].lastIndexOf("@");
+      let lastDotPos = fields["email"].lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          fields["email"].indexOf("@@") == -1 &&
+          lastDotPos > 2 &&
+          fields["email"].length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        errors["email"] = "Email is not valid";
+      }
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  contactSubmit(e) {
+    e.preventDefault();
+
+    // if (this.handleValidation()) {
+    //   alert("Form submitted");
+    // } else {
+    //   alert("Form has errors.");
+    // }
+  }
+
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Sign Up Page</h1>
-        </div>
-    )
+      <div>
+        <form
+          name="contactform"
+          className="contactform"
+          onSubmit={this.contactSubmit.bind(this)}
+        >
+          <div className="col-md-6">
+            <fieldset>
+              <input
+                ref="name"
+                type="text"
+                size="30"
+                placeholder="First Name"
+                onChange={this.handleChange.bind(this, "name")}
+                value={this.state.fields["name"]}
+              />
+              <span style={{ color: "red" }}>{this.state.errors["name"]}</span>
+              <br />
+              <input
+                ref="lastName"
+                type="text"
+                size="30"
+                placeholder="Last Name"
+                onChange={this.handleChange.bind(this, "lastName")}
+                value={this.state.fields["lastName"]}
+              />
+              <span style={{ color: "red" }}>
+                {this.state.errors["lastName"]}
+              </span>
+              <br />
+              <input
+                refs="email"
+                type="text"
+                size="30"
+                placeholder="Email"
+                onChange={this.handleChange.bind(this, "email")}
+                value={this.state.fields["email"]}
+              />
+              <span style={{ color: "red" }}>{this.state.errors["email"]}</span>
+              <br />
+              <input
+                refs="phone"
+                type="text"
+                size="30"
+                placeholder="Phone"
+                onChange={this.handleChange.bind(this, "phone")}
+                value={this.state.fields["phone"]}
+              />
+              <br />
+              <input
+                refs="address"
+                type="text"
+                size="30"
+                placeholder="Address"
+                onChange={this.handleChange.bind(this, "address")}
+                value={this.state.fields["address"]}
+              />
+              <br />
+              <button>Submit</button>
+            </fieldset>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
+// React.render(<SignUp />, document.getElementById("container"));
 export default SignUp;
