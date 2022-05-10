124eb9987688fbb8389bb36cfa988267c6c588b6
Martin Corr <martinjcorr2@gmail.com>
2022-05-10 18:35:53 -0400

    sign up and login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,87 @@
-import React from 'react';
+import React from "react";
 
-function Login() {
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {},
+      errors: {},
+    };
+  }
+
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Userame
+    if (!fields["username"]) {
+      formIsValid = false;
+      errors["username"] = "Cannot be empty";
+    }
+
+    //Password
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  contactSubmit(e) {
+    e.preventDefault();
+
+    if (this.handleValidation()) {
+      alert("Form submitted");
+    } else {
+      alert("Form has errors.");
+    }
+  }
+
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
+
+  render() {
     return (
-        <div>
-            <h1>"Login Page"</h1>
-        </div>
+      <div>
+        <h1>"Login Page"</h1>
+        <form>
+          <fieldset>
+            <input
+              ref="username"
+              type="text"
+              size="30"
+              placeholder="Username"
+              onChange={this.handleChange.bind(this, "username")}
+              value={this.state.fields["username"]}
+            />
+            <span style={{ color: "red" }}>
+              {this.state.errors["username"]}
+            </span>
+            <br />
+            <input
+              ref="password"
+              type="password"
+              size="30"
+              placeholder="Password"
+              onChange={this.handleChange.bind(this, "password")}
+              value={this.state.fields["password"]}
+            />
+            <span style={{ color: "red" }}>
+              {this.state.errors["password"]}
+            </span>
+            <br />
+            <button>Submit</button>
+          </fieldset>
+        </form>
+      </div>
     );
+  }
 }
 export default Login;
