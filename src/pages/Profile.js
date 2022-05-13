9262b136e1c205921fb226751c0e4b780903293c
John Pohl <johnpohl34@gmail.com>
2022-05-12 22:25:03 -0400

    profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Profile() {
   return (
-    <div>
-      <h1>"Profile"</h1>
+    <div className="profile-page">
+      <h1>Your Profile Here.</h1>
     </div>
   );
 }
